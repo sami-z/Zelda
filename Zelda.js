80f089e789ff666ddb8185283b4a7d3ed93cccea
sami-z <samizeremariam@gmail.com>
2021-06-25 14:35:22 -0600

    Place sprite sheet in project directory and begin to splice sprite sheet

--- Zelda.js
+++ Zelda.js
@@ -3,5 +3,42 @@ kaboom({
     fullscreen: true,
     scale: 1,
     debug: true,
+    clearColor: [0,0,0,1]
 })
+
+loadSprite("Link", "Sprites/Link1.png", {
+    // sliceX: -15,
+    // sliceY: 15,
+});
+
+scene('game', (
+// {level, score}
+
+) => {
+
+    const map = [
+       'cxxxx', 
+       'xxxxx', 
+       'xxxxx',
+       'xxxxx', 
+       'xxxxx', 
+       'xxxxx',
+    ]
+
+    const levelCfg = {
+        width: 100,
+        height: 100,
+        'c' : [sprite('Link')]
+
+    }
+
+    addLevel(map, levelCfg)
+
+
+})
+
+
+
+start('game', { level: 0, score: 0})
+
 
